590b4d1a925e32dd76b41422cdbc14b1abbb8460
Stone Gislason <stone@sondevia.is>
2013-06-01 12:59:29 +0100

    calculated minor scale

--- js/generate.js
+++ js/generate.js
@@ -15,11 +15,49 @@ var Generate = {
 
 
 	bass_sequence: function(scale) {
-		return ['F1', 'G1', 'G#1', 'A#1', 'C1', 'C#1', 'D#1', 'F2', 'G2', 'G#2']
+		//return ['F1', 'G1', 'G#1', 'A#1', 'C1', 'C#1', 'D#1', 'F2', 'G2', 'G#2']
+		return Generate.minorscale_note(10, 1);
 	},
 
 	majorscale: function() {
 		var seq = [1,3,5,6,8,10,12];
+	},
+
+	minorscale_note: function(length, offset) {
+		var result = [];
+		var seq = [1,3,4,6,8,9,11];
+		var seq_counter = 0;
+		var padding = 0;
+		var ocatave = 1;
+		for(var i = 0; i < length; i++) {
+
+			if(seq_counter >= seq.length) {
+				seq_counter = 0;
+				padding = padding + 12;
+				ocatave++;
+			}
+			var pos = seq[seq_counter++]+offset;
+			if(pos > 12)
+				pos = pos - 12;
+			result[i] = Generate.note(pos) + ocatave.toString();
+		}
+		return result;
+	},
+
+	minorscale_pos: function(length) {
+		var result = [];
+		var seq = [1,3,4,6,8,9,11];
+		var seq_counter = 0;
+		var padding = 0;
+		for(var i = 0; i < length; i++) {
+
+			if(seq_counter >= seq.length) {
+				seq_counter = 0;
+				padding = padding + 12;
+			}
+			result[i] = padding + seq[seq_counter++];
+		}
+		return result;
 	},
 
 	minorscale: function() {
@@ -45,7 +83,7 @@ var Generate = {
 			case 6:
 				return 'A';
 			case 7:
-				return 'Bb';
+				return 'A#';
 			case 8:
 				return 'B';
 			case 9:
